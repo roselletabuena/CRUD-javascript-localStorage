c467e7f76772a77e767453c8925db355a1793602
Roselle Tabuena <roselleroxtabuena@gmail.com>
2024-02-04 19:20:18 +0800

    add delete functionality

--- index.js
+++ index.js
@@ -11,8 +11,42 @@ closeButton.addEventListener('click', () => {
 });
 
 const studentList = JSON.parse(localStorage.getItem('studentList')) || [];
-
 const studentForm = document.querySelector('#student-form');
+
+const table = document.querySelector('table tbody');
+
+populateTable();
+
+const btnDelete = document.querySelectorAll('.delete-btn');
+const btnEdit = document.querySelectorAll('.edit-btn');
+
+btnEdit.forEach((btn) => {
+  const id = btn.getAttribute('student-id');
+
+  btn.addEventListener('click', () => {
+    dialog.showModal();
+
+    const student = studentList.find((student) => id == student.id);
+
+    document.getElementById('name').value = student.name;
+    document.getElementById('email').value = student.email;
+    document.getElementById('course').value = student.course;
+  });
+});
+
+btnDelete.forEach((btn) => {
+  const id = btn.getAttribute('student-id');
+  const row = btn.parentNode.parentNode;
+  const table = document.querySelector('table');
+
+  btn.addEventListener('click', () => {
+    const newStudentList = studentList.filter((student) => id != student.id);
+
+    table.deleteRow(row.rowIndex);
+
+    localStorage.setItem('studentList', JSON.stringify(newStudentList));
+  });
+});
 
 studentForm.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -20,31 +54,27 @@ studentForm.addEventListener('submit', (event) => {
   const formData = new FormData(studentForm);
   formData.append('id', generateRandomId());
 
-  const data = formDataToObject(formData);
+  const student = formDataToObject(formData);
 
-  studentList.push(data);
-
+  studentList.push(student);
   localStorage.setItem('studentList', JSON.stringify(studentList));
 
-  const table = document.querySelector('table tbody');
-
-  // Insert a new row at the last position of the table
   const newRow = table.insertRow(-1);
-
-  newRow.innerHTML = `
-    <tr>
-    <td>${data.id}</td>
-    <td>${data.name}</td>
-    <td>${data.email}</td>
-    <td>${data.course}</td>
-    <td class="actions">
-      <button class="edit-btn">Edit</button>
-      <button class="delete-btn">Delete</button>
-    </td>
-    </tr>
-
-    `;
+  newRow.innerHTML = tableRowHTML(student);
 });
+
+// READ FUNCTIONALITY
+
+function populateTable() {
+  let tableRow = '';
+  studentList.forEach((student) => {
+    tableRow += tableRowHTML(student);
+  });
+
+  table.innerHTML = tableRow;
+}
+
+// utilities
 
 const generateRandomId = () => Math.floor(Math.random() * 99999);
 
@@ -56,21 +86,15 @@ const formDataToObject = (formData) => {
   return object;
 };
 
-const table = document.querySelector('table tbody');
-
-let tableRow = '';
-
-studentList.forEach(({ id, name, email, course }) => {
-  tableRow += `<tr>
-    <td>${id}</td>
-    <td>${name}</td>
-    <td>${email}</td>
-    <td>${course}</td>
-    <td class="actions">
-      <button class="edit-btn">Edit</button>
-      <button class="delete-btn">Delete</button>
-    </td>
-    </tr>`;
-});
-
-table.innerHTML = tableRow;
+function tableRowHTML(student) {
+  return `<tr>
+            <td>${student.id}</td>
+            <td>${student.name}</td>
+            <td>${student.email}</td>
+            <td>${student.course}</td>
+            <td class="actions">
+            <button student-id="${student.id}" action-type="edit" class="edit-btn">Edit</button>
+            <button student-id="${student.id}" action-type="delete" class="delete-btn">Delete</button>
+            </td>
+        </tr>`;
+}
