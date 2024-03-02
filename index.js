f08347e120ff75aded06a19c34f1db0305373af1
Roselle Tabuena <roselleroxtabuena@gmail.com>
2024-03-02 22:29:44 +0800

    fix bugs and added update, delete functionality

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const dialog = document.querySelector('dialog');
 const showButton = document.querySelector('#btn-add');
 const closeButton = document.querySelector('#btn-close');
 
+const studentList = JSON.parse(localStorage.getItem('studentList')) || [];
+const studentForm = document.getElementById('student-form');
+
 showButton.addEventListener('click', () => {
+  resetForm();
   dialog.showModal();
 });
 
@@ -10,48 +14,77 @@ closeButton.addEventListener('click', () => {
   dialog.close();
 });
 
-const studentList = JSON.parse(localStorage.getItem('studentList')) || [];
-const studentForm = document.querySelector('#student-form');
-
 const table = document.querySelector('table tbody');
 
 populateTable();
 
-const btnDelete = document.querySelectorAll('.delete-btn');
-const btnEdit = document.querySelectorAll('.edit-btn');
-
-btnEdit.forEach((btn) => {
-  const id = btn.getAttribute('student-id');
-
-  btn.addEventListener('click', () => {
+table.addEventListener('click', (event) => {
+  const target = event.target;
+  if (target.classList.contains('edit-btn')) {
+    const id = target.getAttribute('student-id');
     dialog.showModal();
 
     const student = studentList.find((student) => id == student.id);
 
+    document.getElementById('student-id').value = student.id;
     document.getElementById('name').value = student.name;
     document.getElementById('email').value = student.email;
     document.getElementById('course').value = student.course;
-  });
-});
+  } else if (target.classList.contains('delete-btn')) {
+    const id = target.getAttribute('student-id');
+    const row = target.parentNode.parentNode;
+    const newStudentList = (
+      JSON.parse(localStorage.getItem('studentList')) || []
+    ).filter((student) => id != student.id);
 
-btnDelete.forEach((btn) => {
-  const id = btn.getAttribute('student-id');
-  const row = btn.parentNode.parentNode;
-  const table = document.querySelector('table');
-
-  btn.addEventListener('click', () => {
-    const newStudentList = studentList.filter((student) => id != student.id);
-
-    table.deleteRow(row.rowIndex);
+    document.querySelector('table').deleteRow(row.rowIndex);
 
     localStorage.setItem('studentList', JSON.stringify(newStudentList));
-  });
+  }
 });
 
 studentForm.addEventListener('submit', (event) => {
   event.preventDefault();
 
   const formData = new FormData(studentForm);
+
+  const isEditMode = formData.get('id') != '';
+
+  if (isEditMode) {
+    editForm(formData);
+  } else {
+    addForm(formData);
+  }
+
+  resetForm();
+  dialog.close();
+});
+
+// EDIT FUNCTION
+
+const editForm = (formData) => {
+  const studentForm = formDataToObject(formData);
+
+  const studentIndex = studentList.findIndex(
+    (student) => studentForm.id == student.id
+  );
+
+  if (studentIndex == -1) return;
+
+  studentList[studentIndex] = {
+    ...studentList[studentIndex],
+    ...studentForm,
+  };
+
+  localStorage.setItem('studentList', JSON.stringify(studentList));
+
+  document.querySelector('#std_' + studentForm.id).innerHTML =
+    tableRowHTML(studentForm);
+};
+
+// ADD FUNCTION
+
+const addForm = (formData) => {
   formData.append('id', generateRandomId());
 
   const student = formDataToObject(formData);
@@ -60,8 +93,9 @@ studentForm.addEventListener('submit', (event) => {
   localStorage.setItem('studentList', JSON.stringify(studentList));
 
   const newRow = table.insertRow(-1);
+  newRow.id = 'std_' + student.id;
   newRow.innerHTML = tableRowHTML(student);
-});
+};
 
 // READ FUNCTIONALITY
 
@@ -75,7 +109,6 @@ function populateTable() {
 }
 
 // utilities
-
 const generateRandomId = () => Math.floor(Math.random() * 99999);
 
 const formDataToObject = (formData) => {
@@ -87,7 +120,7 @@ const formDataToObject = (formData) => {
 };
 
 function tableRowHTML(student) {
-  return `<tr>
+  return `<tr id='std_${student.id}'>
             <td>${student.id}</td>
             <td>${student.name}</td>
             <td>${student.email}</td>
@@ -97,4 +130,9 @@ function tableRowHTML(student) {
             <button student-id="${student.id}" action-type="delete" class="delete-btn">Delete</button>
             </td>
         </tr>`;
+}
+
+function resetForm() {
+  document.querySelector('#student-id').value = '';
+  studentForm.reset();
 }
